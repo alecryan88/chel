bbcef9c746c2af682eb3b885dd4a740dee0f3ca2
flyers88 <aryan@themeetgroup.com>
2022-04-26 08:54:34 -0400

    Get asset event run_id from upstream asset in dbt sensor

--- nhl_elt/sensors/dbt_sensor.py
+++ nhl_elt/sensors/dbt_sensor.py
@@ -9,7 +9,14 @@ def create_dbt_asset_sensor(key, dbt_run):
         job=dbt_run
     )
     def dbt_sensor(context, asset_event):
-        asset_partition = asset_event.dagster_event.partition
+        
+        upstream_run = context.instance.get_run_by_id(asset_event.run_id)
+
+        #Get the run config of the upstream asset_event
+        upstream_run_config = upstream_run.run_config
+
+        #Parse out the run dat from the upstream asset_event run_config
+        run_date = upstream_run_config['resources']['run_parameters']['config']['run_date']
 
         yield RunRequest(
             run_key=context.cursor,
@@ -19,7 +26,7 @@ def create_dbt_asset_sensor(key, dbt_run):
                         "config": {
                             "profiles_dir" : "dbt",
                             "project_dir" : "dbt",
-                            "var" : { 'run_date' : asset_partition}
+                            "var" : { 'run_date' : run_date}
                         }
                     }
                 }
