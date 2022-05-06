5b9f06efd25cdbd45a70a6e9c3aa4f53e3d0dfdf
flyers88 <aryan@themeetgroup.com>
2022-05-06 08:43:21 -0400

    Docker compose

--- nhl_dagster/jobs/dbt_transforms.py
+++ nhl_dagster/jobs/dbt_transforms.py
@@ -2,7 +2,7 @@ from dagster import AssetGroup
 from dagster_dbt import dbt_cli_resource
 from dagster_dbt.asset_defs import load_assets_from_dbt_project
 
-DBT_DIR = '/opt/dagster/app/nhl_dbt'
+DBT_DIR = 'nhl_dbt'
 
 dbt_assets = load_assets_from_dbt_project(project_dir = DBT_DIR, profiles_dir = DBT_DIR)
 
