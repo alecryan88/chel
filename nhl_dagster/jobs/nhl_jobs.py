2dfc6a13c106b945918ab7c0936371775891fdba
flyers88 <aryan@themeetgroup.com>
2022-06-02 08:16:19 -0400

    Add graph logic

--- nhl_dagster/jobs/nhl_jobs.py
+++ nhl_dagster/jobs/nhl_jobs.py
@@ -1,11 +1,23 @@
-from dagster import job, op, get_dagster_logger, In, Nothing, ResourceDefinition
+from dagster import graph, ResourceDefinition
 from nhl_dagster.ops.s3_ops import *
 from nhl_dagster.ops.dbt_ops import *
 from nhl_dagster.ops.snowflake_ops import *
 from nhl_dagster.partitions.daily_partitions import nhl_elt_daily_config
 from nhl_dagster.resources.resources import * 
 
-@job(
+
+@graph 
+def nhl_elt():
+    game_id_list = extract_game_ids_to_list()
+    load_data = load_game_data_to_s3(game_id_list)
+    delete_partition = delete_partition_from_snowflake(start=load_data)
+    copy_partition = copy_partition_into_snowflake(start=delete_partition)
+    dbt_run_models = dbt_run(start=copy_partition)
+    dbt_test_models = dbt_test(start=dbt_run_models)
+    dbt_load_manifest = load_dbt_manifest_to_s3(start=dbt_test_models)
+    
+
+nhl_elt_job = nhl_elt.to_job(
     config=nhl_elt_daily_config,
     resource_defs={
         "s3": s3_resource,
@@ -14,11 +26,3 @@ from nhl_dagster.resources.resources import *
         "run_date":ResourceDefinition.string_resource()
         }
     )
-def run_elt():
-    game_id_list = extract_game_ids_to_list()
-    load_data = load_game_data_to_s3(game_id_list)
-    delete_partition = delete_partition_from_snowflake(start=load_data)
-    copy_partition = copy_partition_into_snowflake(start=delete_partition)
-    dbt_run_models = dbt_run(start=copy_partition)
-    dbt_test_models = dbt_test(start=dbt_run_models)
-    dbt_load_manifest = load_dbt_manifest_to_s3(start=dbt_test_models)
--- nhl_dagster/repo.py
+++ nhl_dagster/repo.py
@@ -1,12 +1,12 @@
 from dagster import repository, build_schedule_from_partitioned_job
-from nhl_dagster.jobs.nhl_jobs import run_elt
+from nhl_dagster.jobs.nhl_jobs import nhl_elt_job
 
 
 @repository
 def nhl_elt():
     return [
         build_schedule_from_partitioned_job(
-            run_elt,
+            nhl_elt_job,
             description = 'Downloads NHL API data to s3, copies from snowflake and builds dbt models.',
             hour_of_day = 4,
             minute_of_hour = 56
