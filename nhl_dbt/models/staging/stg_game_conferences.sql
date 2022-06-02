b6d4c43fb505068bf3b78181db198a465f6103ba
flyers88 <aryan@themeetgroup.com>
2022-06-02 08:49:49 -0400

    Remove run started at

--- nhl_dbt/models/staging/stg_game_conferences.sql
+++ nhl_dbt/models/staging/stg_game_conferences.sql
@@ -10,8 +10,7 @@ Select distinct
     JSON_EXTRACT:gameData:game:pk::string as game_id,
     teams.value:conference.id::integer as conference_id,
     teams.value:conference.name::string as conference_name,
-    teams.value:division.id::int as division_id,
-    '{{ run_started_at }}' as last_updated_dbt
+    teams.value:division.id::int as division_id
    
 from {{source('NHL_DB_RAW', 'RAW_NHL_GAME_DATA')}}, table(flatten(JSON_EXTRACT:gameData:teams)) teams
 
