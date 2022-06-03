2ef8a9de1f950f0d8f00516bc35eb08be88acf72
flyers88 <aryan@themeetgroup.com>
2022-06-03 15:31:35 -0400

    Add secrets

--- nhl_dbt_cd/download_manifest_from_s3.py
+++ nhl_dbt_cd/download_manifest_from_s3.py
@@ -1,11 +1,7 @@
 import boto3
 import os
  
-s3_client = boto3.client('s3', 
-                      aws_access_key_id='AKIAZNPFTOMRZY4URRGB',
-                      aws_secret_access_key='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',
-                      region_name='us-east-1'
-                      )
+s3_client = boto3.client('s3')
 
 filename = 'manifest.json'
 location = 'nhl_dbt/ci_manifest/manifest.json'
