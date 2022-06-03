450a907195bd7767756604d5ffda152d1e4b618b
flyers88 <aryan@themeetgroup.com>
2022-06-02 20:39:37 -0400

    Fix filename

--- nhl_dbt_cd/download_manifest_from_s3.py
+++ nhl_dbt_cd/download_manifest_from_s3.py
@@ -1,17 +1,13 @@
 import boto3
 import os
  
-s3_client = boto3.client('s3', 
-                      aws_access_key_id='AKIAZNPFTOMRZY4URRGB',
-                      aws_secret_access_key='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',
-                      region_name='us-east-1'
-                      )
+s3_client = boto3.client('s3')
 
-file = 'manifest.json'
+filename = 'manifest.json'
 location = 'nhl_dbt/ci_manifest/manifest.json'
 
 bucket = 'nhl-prod-dbt-manifest'
 
-s3_client.download_file(bucket, file, location)
+s3_client.download_file(bucket, filename, location)
 
 print("Success.")
